571c7d217eeb88e871aa69d012571f5c0eefbfbc
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-07-06 01:45:54 -0400

    Add vscode helper and fix formatting in react app

--- webview-ui/src/App.tsx
+++ webview-ui/src/App.tsx
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import logo from "./logo.svg"
+import "./App.css"
+
+import { vscode } from "./utilities/vscode"
+import { VSCodeButton } from "@vscode/webview-ui-toolkit/react"
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	function handleHowdyClick() {
+		vscode.postMessage({
+			command: "hello",
+			text: "Hey there partner! 🤠",
+		})
+	}
+
+	return (
+		<main>
+			<h1>Hello World!</h1>
+			<VSCodeButton onClick={handleHowdyClick}>Howdy!</VSCodeButton>
+		</main>
+	)
 }
 
-export default App;
+export default App
