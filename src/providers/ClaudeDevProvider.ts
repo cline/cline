7c82badf3c43fe342032d9b4959da6f98f9231da
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-07-28 11:50:31 -0400

    Update view IDs and add comment about vscode caching behavior

--- src/providers/ClaudeDevProvider.ts
+++ src/providers/ClaudeDevProvider.ts
@@ -13,7 +13,8 @@ https://github.com/KumarVariable/vscode-extension-sidebar-html/blob/master/src/c
 */
 
 export class ClaudeDevProvider implements vscode.WebviewViewProvider {
-	public static readonly viewType = "claude-dev.SidebarProvider"
+	public static readonly sideBarId = "claude-dev.SidebarProvider" // used in package.json as the view's id. This value cannot be changed due to how vscode caches views based on their id, and updating the id would break existing instances of the extension.
+	public static readonly tabPanelId = "claude-dev.TabPanelProvider"
 	private disposables: vscode.Disposable[] = []
 	private view?: vscode.WebviewView | vscode.WebviewPanel
 	private providerInstanceIdentifier = Date.now()
