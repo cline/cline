b32f91b0ee6efdaf36d81e1b1d3642ff8790505f
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-08-03 15:46:04 -0400

    Remove log

--- webview-ui/src/utilities/getLanguageFromPath.ts
+++ webview-ui/src/utilities/getLanguageFromPath.ts
@@ -85,6 +85,5 @@ const extensionToLanguage: { [key: string]: string } = {
 
 export function getLanguageFromPath(path: string): string | undefined {
 	const extension = path.split(".").pop()?.toLowerCase() || ""
-    console.log(`converted path to language: ${path} -> ${extensionToLanguage[extension]}`)
 	return extensionToLanguage[extension]
 }
