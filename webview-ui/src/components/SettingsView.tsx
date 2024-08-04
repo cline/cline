e29ef7e2f1b28a24aec81ef205e8e10b96e6db1e
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-08-04 19:47:04 -0400

    Update TaskHeader

--- webview-ui/src/components/SettingsView.tsx
+++ webview-ui/src/components/SettingsView.tsx
@@ -129,7 +129,7 @@ const SettingsView = ({
 						https://github.com/saoudrizwan/claude-dev
 					</VSCodeLink>
 				</p>
-				<p style={{ fontStyle: "italic" }}>v1.0.92</p>
+				<p style={{ fontStyle: "italic" }}>v1.0.93</p>
 			</div>
 		</div>
 	)
