0f341131fae8449686ad8ebe6f23ce26745a3467
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-08-31 07:14:52 -0400

    Add announcement

--- webview-ui/src/components/Announcement.tsx
+++ webview-ui/src/components/Announcement.tsx
@@ -31,6 +31,11 @@ const Announcement = ({ version, hideAnnouncement, apiConfiguration, vscodeUriSc
 			</h3>
 			<ul style={{ margin: "0 0 8px", paddingLeft: "12px" }}>
 				<li>
+					<b>You can now edit Claude's changes before accepting!</b> When he edits or creates a file, you can
+					modify his changes directly in the right side of the diff view (+ hover over the 'Revert Block'
+					arrow button in the center to undo "<code>{"// rest of code here"}</code>" shenanigans)
+				</li>
+				<li>
 					Adds support for reading .pdf and .docx files (try "turn my business_plan.docx into a company
 					website")
 				</li>
@@ -41,11 +46,6 @@ const Announcement = ({ version, hideAnnouncement, apiConfiguration, vscodeUriSc
 				<li>
 					Adds "Always allow read-only operations" setting to let Claude read files and view directories
 					without needing to approve (<b>off</b> by default).
-				</li>
-				<li>Adds sliding window context management to keep tasks going past 200k tokens.</li>
-				<li>
-					Adds Google Cloud Vertex AI support and updates Claude 3.5 Sonnet max output to 8192 tokens for all
-					providers.
 				</li>
 			</ul>
 			<p style={{ margin: "0" }}>
