1429970558f0e2c8f93bfab3d4cee555cee6092b
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-09-09 13:34:26 -0400

    Update prompt

--- src/ClaudeDev.ts
+++ src/ClaudeDev.ts
@@ -1782,7 +1782,7 @@ ${
 				details += `\n## ${busyTerminal.lastCommand}`
 				const newOutput = this.terminalManager.getUnretrievedOutput(busyTerminal.id)
 				if (newOutput) {
-					details += `\nNew Output:\n${newOutput}`
+					details += `\n### New Output\n${newOutput}`
 				} else {
 					// details += `\n(Still running, no new output)` // don't want to show this right after running the command
 				}
