c84970b99444d9e3a04671833613969ef561acc9
Peter Stalman <sarkedev@gmail.com>
2024-09-25 01:32:19 -0700

    fix formatting and refactor Settings.ts

--- src/core/Settings.ts
+++ src/core/Settings.ts
@@ -1,21 +1,34 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode"
+
+type SettingKey =
+	| "alwaysAllowReadOnly"
 
 export class Settings {
-    private static instance: Settings;
-    private constructor() {}
+	private static instance: Settings
+	private constructor() {}
 
-    public static getInstance(): Settings {
-        if (!Settings.instance) {
-            Settings.instance = new Settings();
-        }
-        return Settings.instance;
-    }
+	public static getInstance(): Settings {
+		if (!Settings.instance) {
+			Settings.instance = new Settings()
+		}
+		return Settings.instance
+	}
 
-    public get alwaysAllowReadOnly(): boolean {
-        return vscode.workspace.getConfiguration('claudeDev').get('alwaysAllowReadOnly', false);
-    }
+	private config = () => vscode.workspace.getConfiguration("claudeDev")
 
-    public set alwaysAllowReadOnly(value: boolean) {
-        vscode.workspace.getConfiguration('claudeDev').update('alwaysAllowReadOnly', value, vscode.ConfigurationTarget.Global);
-    }
+	private getValue<T>(key: SettingKey, defaultValue: T): T {
+		return this.config().get(key, defaultValue)
+	}
+
+	private setValue<T>(key: SettingKey, value: T): Thenable<void> {
+		return this.config().update(key, value, vscode.ConfigurationTarget.Global)
+	}
+
+	public get alwaysAllowReadOnly(): boolean {
+		return this.getValue("alwaysAllowReadOnly", false)
+	}
+
+	public set alwaysAllowReadOnly(value: boolean) {
+		this.setValue("alwaysAllowReadOnly", value)
+	}
 }
