6502b3cb244adaeafe628907087b1afadc36b8a8
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-10-06 00:02:13 -0400

    Rename Cline

--- src/extension.ts
+++ src/extension.ts
@@ -20,24 +20,10 @@ let outputChannel: vscode.OutputChannel
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	//console.log('Congratulations, your extension "claude-dev" is now active!')
-
 	outputChannel = vscode.window.createOutputChannel("Cline")
 	context.subscriptions.push(outputChannel)
 
 	outputChannel.appendLine("Cline extension activated")
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	// const disposable = vscode.commands.registerCommand("claude-dev.helloWorld", () => {
-	// 	// The code you place here will be executed every time your command is executed
-	// 	// Display a message box to the user
-	// 	vscode.window.showInformationMessage("Hello World from claude-dev!")
-	// })
-	// context.subscriptions.push(disposable)
 
 	const sidebarProvider = new ClineProvider(context, outputChannel)
 
@@ -94,7 +80,6 @@ export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(
 		vscode.commands.registerCommand("claude-dev.settingsButtonTapped", () => {
-			//const message = "claude-dev.settingsButtonTapped!"
 			//vscode.window.showInformationMessage(message)
 			sidebarProvider.postMessageToWebview({ type: "action", action: "settingsButtonTapped" })
 		})
