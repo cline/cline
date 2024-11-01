5adf96dc82b90474ab8455bcbba2dac072747863
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2024-10-31 20:26:04 -0400

    Fixes

--- src/core/Cline.ts
+++ src/core/Cline.ts
@@ -1032,16 +1032,18 @@ export class Cline {
 							newContent = newContent.split("\n").slice(0, -1).join("\n").trim()
 						}
 
-						// it seems not just llama models are doing this, but also gemini and potentially others
-						if (
-							newContent.includes("&gt;") ||
-							newContent.includes("&lt;") ||
-							newContent.includes("&quot;")
-						) {
-							newContent = newContent
-								.replace(/&gt;/g, ">")
-								.replace(/&lt;/g, "<")
-								.replace(/&quot;/g, '"')
+						if (!this.api.getModel().id.includes("claude")) {
+							// it seems not just llama models are doing this, but also gemini and potentially others
+							if (
+								newContent.includes("&gt;") ||
+								newContent.includes("&lt;") ||
+								newContent.includes("&quot;")
+							) {
+								newContent = newContent
+									.replace(/&gt;/g, ">")
+									.replace(/&lt;/g, "<")
+									.replace(/&quot;/g, '"')
+							}
 						}
 
 						const sharedMessageProps: ClineSayTool = {
