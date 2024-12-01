b9c274add24a3caca484658e183c3b65677ce462
lloydchang <lloydchang@gmail.com>
2024-12-01 14:31:58 -0800

    feat(extension.ts): use rocket icon (#34)

--- src/extension.ts
+++ src/extension.ts
@@ -76,8 +76,8 @@ export function activate(context: vscode.ExtensionContext) {
 		// TODO: use better svg icon with light and dark variants (see https://stackoverflow.com/questions/58365687/vscode-extension-iconpath)
 
 		panel.iconPath = {
-			light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "robot_panel_light.png"),
-			dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "robot_panel_dark.png"),
+			light: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "rocket.png"),
+			dark: vscode.Uri.joinPath(context.extensionUri, "assets", "icons", "rocket.png"),
 		}
 		tabProvider.resolveWebviewView(panel)
 
