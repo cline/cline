f5bd5fae0913b31095b117382239a02f4144c371
Matt Rubens <mrubens@users.noreply.github.com>
2024-12-17 07:50:53 -0500

    Prompt tweak for gemini line numbers

--- src/core/prompts/system.ts
+++ src/core/prompts/system.ts
@@ -61,7 +61,7 @@ Usage:
 Description: Request to write full content to a file at the specified path. If the file exists, it will be overwritten with the provided content. If the file doesn't exist, it will be created. This tool will automatically create any directories needed to write the file.
 Parameters:
 - path: (required) The path of the file to write to (relative to the current working directory ${cwd.toPosix()})
-- content: (required) The content to write to the file. ALWAYS provide the COMPLETE intended content of the file, without any truncation or omissions. You MUST include ALL parts of the file, even if they haven't been modified.
+- content: (required) The content to write to the file. ALWAYS provide the COMPLETE intended content of the file, without any truncation or omissions. You MUST include ALL parts of the file, even if they haven't been modified. Do NOT include the line numbers in the content though, just the actual content of the file.
 Usage:
 <write_to_file>
 <path>File path here</path>
