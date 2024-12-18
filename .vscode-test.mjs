a12ba47c857f74b5c1a36eaff1e7b3f3195463c0
Mark Percival <m@mdp.im>
2024-12-18 16:15:20 -0500

    Fix: Add basic CI testing

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -1,5 +1,9 @@
 import { defineConfig } from "@vscode/test-cli"
 
-export default defineConfig({
-	files: "out/test/**/*.test.js",
-})
+export default defineConfig([
+	{
+		label: "VSCode Extension Tests",
+		files: "out/src/test/vscode/*.test.js",
+		version: "stable",
+	},
+])
