1ce57f22031758c3a1fbb504ee59688b999c4f22
Hanzen Shou <shouhanzen@gmail.com>
2024-12-28 21:04:25 -0800

    doc: linked issue to markdown plugin

--- webview-ui/src/components/common/MarkdownBlock.tsx
+++ webview-ui/src/components/common/MarkdownBlock.tsx
@@ -53,6 +53,7 @@ const remarkUrlToLink = () => {
 /**
  * Custom remark plugin that prevents filenames with extensions from being parsed as bold text
  * For example: __init__.py should not be rendered as bold "init" followed by ".py"
+ * Solves https://github.com/cline/cline/issues/1028
  */
 const remarkPreventBoldFilenames = () => {
 	return (tree: any) => {
