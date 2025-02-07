abc66fbaa7207e16d49cfa93ccac7a71126c6053
yaojunWang <gooqle.com.hk@gmail.com>
2025-02-07 11:38:50 +0800

    fix problem what is when you use Qwen api provider and then you want to change the api provider ,the apiline dropdown will obscure your api provider drop-down options

--- webview-ui/src/components/settings/ApiOptions.tsx
+++ webview-ui/src/components/settings/ApiOptions.tsx
@@ -315,7 +315,7 @@ const ApiOptions = ({ showModelOptions, apiErrorMessage, modelIdErrorMessage, is
 
 			{selectedProvider === "qwen" && (
 				<div>
-					<DropdownContainer className="dropdown-container">
+					<DropdownContainer className="dropdown-container" style={{position: "inherit"}}>
 						<label htmlFor="qwen-line-provider">
 							<span style={{ fontWeight: 500, marginTop: 5 }}>Alibaba API Line</span>
 						</label>
