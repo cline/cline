4375d1562ff160509e393fb022a2cb44041b5133
brownrw8 <rwbrown@roadrunner.com>
2025-02-18 08:11:52 -1000

    remove imports

--- webview-ui/src/components/settings/__tests__/APIOptions.spec.tsx
+++ webview-ui/src/components/settings/__tests__/APIOptions.spec.tsx
@@ -1,9 +1,7 @@
-import React from "react"
-import { render, screen, waitFor } from "@testing-library/react"
+import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import ApiOptions from "../ApiOptions"
 import { ExtensionStateContextProvider } from "../../../context/ExtensionStateContext"
-import { vscode } from "../../../utils/vscode"
 
 vi.mock("../src/utils/vscode", () => ({
 	vscode: {
