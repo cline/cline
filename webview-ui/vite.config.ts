bff0e86e65a7b56337ed8529c3663f1a6c0297aa
frostbournesb <frostbournesb@protonmail.com>
2025-02-18 23:10:03 -0500

    Make vite work

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -1,7 +1,7 @@
 /// <reference types="vitest/config" />
 
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react-swc"
 
 export default defineConfig({
 	plugins: [react()],
@@ -9,5 +9,26 @@ export default defineConfig({
 		environment: "jsdom",
 		globals: true,
 		setupFiles: ["./setupTests.js"],
+	},
+	build: {
+		outDir: "build",
+		rollupOptions: {
+			output: {
+				entryFileNames: `assets/[name].js`,
+				chunkFileNames: `assets/[name].js`,
+				assetFileNames: `assets/[name].[ext]`,
+			},
+		},
+	},
+	server: {
+		hmr: {
+			host: "localhost",
+			protocol: "ws",
+		},
+		cors: {
+			origin: "*",
+			methods: "*",
+			allowedHeaders: "*",
+		},
 	},
 })
