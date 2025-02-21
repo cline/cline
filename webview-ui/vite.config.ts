713dcf27509657e4fcd4d7b9400122145bc9c6f5
frostbournesb <frostbournesb@protonmail.com>
2025-02-21 05:14:51 -0500

    Add IS_DEV to env definitions

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -34,4 +34,10 @@ export default defineConfig({
 			allowedHeaders: "*",
 		},
 	},
+	define: {
+		"process.env": {
+			NODE_ENV: JSON.stringify(process.env.IS_DEV ? "development" : "production"),
+			IS_DEV: JSON.stringify(process.env.IS_DEV),
+		},
+	},
 })
