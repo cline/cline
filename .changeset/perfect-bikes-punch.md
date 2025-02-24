74f97e73c253f7635f230dc11a3bb214060f3241
Flavio Pellanda <flavio.pellanda@inacta.ch>
2025-02-24 13:00:05 +0100

    fix: Api configuration error not display on Welcome Screen

---
+++ .changeset/perfect-bikes-punch.md
@@ -0,0 +1,5 @@
+---
+"claude-dev": patch
+---
+
+Fix api configuration error not display on Welcome Screen
--- webview-ui/src/components/welcome/WelcomeView.tsx
+++ webview-ui/src/components/welcome/WelcomeView.tsx
@@ -107,7 +107,7 @@ const WelcomeView = () => {
 				</div>
 
 				<div style={{ marginTop: "15px" }}>
-					<ApiOptions showModelOptions={false} />
+					<ApiOptions showModelOptions={false} apiErrorMessage={apiErrorMessage} />
 					<VSCodeButton onClick={handleSubmit} disabled={disableLetsGoButton} style={{ marginTop: "3px" }}>
 						Let's go!
 					</VSCodeButton>
