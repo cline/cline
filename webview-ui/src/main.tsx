34e87af5f1bf171a4d3d4b6b76bdc33c89860c45
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2025-02-28 22:24:34 -0800

    Remove webview telemetry

--- webview-ui/src/main.tsx
+++ webview-ui/src/main.tsx
@@ -5,13 +5,8 @@ import "./index.css"
 import App from "./App.tsx"
 import "../../node_modules/@vscode/codicons/dist/codicon.css"
 
-const apiKey = "phc_qfOAGxZw2TL5O8p9KYd9ak3bPBFzfjC8fy5L6jNWY7K"
-const apiHost = "https://us.i.posthog.com"
-
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
-		<PostHogProvider apiKey={apiKey} options={{ api_host: apiHost }}>
-			<App />
-		</PostHogProvider>
+		<App />
 	</StrictMode>,
 )
