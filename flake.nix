a53206912c849abb0cc640db351d95bbe1fb10e4
Wojciech Kordalski <wkordalski@pm.me>
2025-03-15 11:27:33 +0100

    Did you know about https://direnv.net/
    It is a nice tool that activates nix flake dev shells when entering project directory.
    Unfortunatelly it enters infinite loop when shellHook executes another inner zsh shell.

--- flake.nix
+++ flake.nix
@@ -16,14 +16,9 @@
       name = "roo-code";
       
       packages = with pkgs; [
-        zsh
         nodejs_18
         corepack_18
       ];
-
-      shellHook = ''
-        exec zsh
-      '';
     };
   in {
     devShells = forAllSystems (system: {
