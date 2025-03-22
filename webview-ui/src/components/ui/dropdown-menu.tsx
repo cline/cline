92dee31634039d8a7826ec961eaba986ffd38645
KJ7LNW <93454819+KJ7LNW@users.noreply.github.com>
2025-03-22 16:01:27 -0700

    fix: make dropdown menu background fully opaque (#1907)

--- webview-ui/src/components/ui/dropdown-menu.tsx
+++ webview-ui/src/components/ui/dropdown-menu.tsx
@@ -27,7 +27,7 @@ const DropdownMenuContent = React.forwardRef<
 				"z-50 min-w-[8rem] overflow-hidden rounded-xs p-1 shadow-xs",
 				"data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
 				"border border-vscode-focusBorder",
-				"bg-vscode-dropdown-background",
+				"bg-vscode-dropdown-background bg-opacity-100",
 				"text-vscode-dropdown-foreground",
 				className,
 			)}
