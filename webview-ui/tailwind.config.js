0d5c5d347b33e0c83002e960cd4a326a1a2a85d8
frostbournesb <frostbournesb@protonmail.com>
2025-03-25 21:11:44 -0700

    make heroui work

---
+++ webview-ui/tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+const { heroui } = require("@heroui/react")
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	darkMode: "class",
+	plugins: [heroui()],
+}
