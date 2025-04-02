1b25190947189d87470469548b765fc5f7e29038
Smartsheet-JB-Brown <jb.brown@smartsheet.com>
2025-04-02 08:59:51 -0700

    fixes: 2224 - Update AWS region drop down to be current. (#2225)

--- webview-ui/src/components/settings/constants.ts
+++ webview-ui/src/components/settings/constants.ts
@@ -39,22 +39,30 @@ export const PROVIDERS = [
 	{ value: "human-relay", label: "Human Relay" },
 ].sort((a, b) => a.label.localeCompare(b.label))
 
+//This list alpha sorted and updated April 2, 2025 to include any region that supported 1 or
+//more models shown at https://docs.aws.amazon.com/bedrock/latest/userguide/models-regions.html
+
 export const AWS_REGIONS = [
-	{ value: "us-east-1", label: "us-east-1" },
-	{ value: "us-east-2", label: "us-east-2" },
-	{ value: "us-west-2", label: "us-west-2" },
-	{ value: "ap-south-1", label: "ap-south-1" },
 	{ value: "ap-northeast-1", label: "ap-northeast-1" },
 	{ value: "ap-northeast-2", label: "ap-northeast-2" },
+	{ value: "ap-south-1", label: "ap-south-1" },
 	{ value: "ap-southeast-1", label: "ap-southeast-1" },
 	{ value: "ap-southeast-2", label: "ap-southeast-2" },
 	{ value: "ca-central-1", label: "ca-central-1" },
 	{ value: "eu-central-1", label: "eu-central-1" },
+	{ value: "eu-central-2", label: "eu-central-2" },
+	{ value: "eu-north-1", label: "eu-north-1" },
+	{ value: "eu-south-1", label: "eu-south-1" },
+	{ value: "eu-south-2", label: "eu-south-2" },
 	{ value: "eu-west-1", label: "eu-west-1" },
 	{ value: "eu-west-2", label: "eu-west-2" },
 	{ value: "eu-west-3", label: "eu-west-3" },
 	{ value: "sa-east-1", label: "sa-east-1" },
+	{ value: "us-east-1", label: "us-east-1" },
+	{ value: "us-east-2", label: "us-east-2" },
+	{ value: "us-gov-east-1", label: "us-gov-east-1" },
 	{ value: "us-gov-west-1", label: "us-gov-west-1" },
+	{ value: "us-west-2", label: "us-west-2" },
 ]
 
 export const VERTEX_REGIONS = [
