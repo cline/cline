a0c1f0ecab01dbd7089e483067440ed8ee82d319
pashpashpash <nik@nugbase.com>
2025-04-04 12:05:39 -0700

    better styling for the new task tool

--- webview-ui/src/components/chat/NewTaskPreview.tsx
+++ webview-ui/src/components/chat/NewTaskPreview.tsx
@@ -7,8 +7,8 @@ interface NewTaskPreviewProps {
 
 const NewTaskPreview: React.FC<NewTaskPreviewProps> = ({ context }) => {
 	return (
-		<div>
-			<MarkdownBlock markdown={"```\n" + context + "\n```"} />
+		<div className="bg-[var(--vscode-badge-background)] text-[var(--vscode-badge-foreground)] rounded-[3px] p-[9px] pt-0 border-t-8 border-[var(--vscode-charts-green)]">
+			<MarkdownBlock markdown={context} />
 		</div>
 	)
 }
