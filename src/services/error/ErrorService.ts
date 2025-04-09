49d3bcfedce2b791f50497940bb94a887a149cbd
Dennis Bartlett <bartlett.dc.1@gmail.com>
2025-04-09 16:51:58 -0700

    Update sentry in extension to add traceability and version  (#2775)
    * Add Sentry package
    
    * Setup Logger to use Error Service
    
    * Enhance traceability and message information

--- src/services/error/ErrorService.ts
+++ src/services/error/ErrorService.ts
@@ -1,8 +1,12 @@
 import * as Sentry from "@sentry/browser"
+import * as pkg from "../../../package.json"
 
 // Initialize sentry
 Sentry.init({
 	dsn: "https://7936780e3f0f0290fcf8d4a395c249b7@o4509028819664896.ingest.us.sentry.io/4509052955983872",
+	environment: process.env.NODE_ENV,
+	release: `cline@${pkg.version}`,
+	integrations: [Sentry.browserTracingIntegration(), Sentry.replayIntegration()],
 })
 
 export class ErrorService {
