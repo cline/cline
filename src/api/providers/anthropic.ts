b073ffd3282d94b9b6ec9bedcc626008044d0747
atsushi-ishibashi <atsushi.ishibashi@finatext.com>
2025-04-13 05:39:45 +0900

    chore: typo

--- src/api/providers/anthropic.ts
+++ src/api/providers/anthropic.ts
@@ -34,7 +34,7 @@ export class AnthropicHandler implements ApiHandler {
 			case "claude-3-opus-20240229":
 			case "claude-3-haiku-20240307": {
 				/*
-				The latest message will be the new user message, one before will be the assistant message from a previous request, and the user message before that will be a previously cached user message. So we need to mark the latest user message as ephemeral to cache it for the next request, and mark the second ant the third to last user message as ephemeral to let the server know the last message to retrieve from the cache for the current request..
+				The latest message will be the new user message, one before will be the assistant message from a previous request, and the user message before that will be a previously cached user message. So we need to mark the latest user message as ephemeral to cache it for the next request, and mark the second and the third to last user message as ephemeral to let the server know the last message to retrieve from the cache for the current request..
 				*/
 				const userMsgIndices = messages.reduce(
 					(acc, msg, index) => (msg.role === "user" ? [...acc, index] : acc),
