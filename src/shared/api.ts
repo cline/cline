96ff9fc380ee1f627ede849b8589a422f401fb3a
Chris Estreich <cestreich@gmail.com>
2025-04-18 14:43:27 -0700

    Fix pricing for Gemini 2.5 Flash (Thinking) (#2773)
    * Fix pricing for Gemini 2.5 Flash (Thinking)
    
    * Looks like it's actually $3.50
    
    * We aren't honoring custom thinking token budgets on Vertex yet

--- src/shared/api.ts
+++ src/shared/api.ts
@@ -477,24 +477,6 @@ export const openRouterDefaultModelInfo: ModelInfo = {
 export type VertexModelId = keyof typeof vertexModels
 export const vertexDefaultModelId: VertexModelId = "claude-3-7-sonnet@20250219"
 export const vertexModels = {
-	"gemini-2.0-flash-001": {
-		maxTokens: 8192,
-		contextWindow: 1_048_576,
-		supportsImages: true,
-		supportsPromptCache: false,
-		inputPrice: 0.15,
-		outputPrice: 0.6,
-	},
-	"gemini-2.5-flash-preview-04-17:thinking": {
-		maxTokens: 65_535,
-		contextWindow: 1_048_576,
-		supportsImages: true,
-		supportsPromptCache: false,
-		inputPrice: 0.15,
-		outputPrice: 0.6,
-		thinking: true,
-		maxThinkingTokens: 24_576,
-	},
 	"gemini-2.5-flash-preview-04-17": {
 		maxTokens: 65_535,
 		contextWindow: 1_048_576,
@@ -502,7 +484,6 @@ export const vertexModels = {
 		supportsPromptCache: false,
 		inputPrice: 0.15,
 		outputPrice: 0.6,
-		thinking: false,
 	},
 	"gemini-2.5-pro-preview-03-25": {
 		maxTokens: 65_535,
@@ -527,6 +508,14 @@ export const vertexModels = {
 		supportsPromptCache: false,
 		inputPrice: 0,
 		outputPrice: 0,
+	},
+	"gemini-2.0-flash-001": {
+		maxTokens: 8192,
+		contextWindow: 1_048_576,
+		supportsImages: true,
+		supportsPromptCache: false,
+		inputPrice: 0.15,
+		outputPrice: 0.6,
 	},
 	"gemini-2.0-flash-lite-001": {
 		maxTokens: 8192,
@@ -657,7 +646,7 @@ export const geminiModels = {
 		supportsImages: true,
 		supportsPromptCache: false,
 		inputPrice: 0.15,
-		outputPrice: 0.6,
+		outputPrice: 3.5,
 		thinking: true,
 		maxThinkingTokens: 24_576,
 	},
