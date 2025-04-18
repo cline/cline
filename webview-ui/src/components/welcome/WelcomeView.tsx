260c3617b8c9b1b66fc7f9cffd2f94851e75b1b6
narunbabu <narun.iitb@gmail.com>
2025-04-18 13:22:41 +0530

    format changes

--- webview-ui/src/components/welcome/WelcomeView.tsx
+++ webview-ui/src/components/welcome/WelcomeView.tsx
@@ -28,7 +28,7 @@ const WelcomeView = memo(() => {
 	return (
 		<div className="fixed inset-0 p-0 flex flex-col">
 			<div className="h-full px-5 overflow-auto">
-				<h2>Hi, I'm Cline</h2>
+				<h2>Hi, I'm Cline-Arun-3</h2>
 				<div className="flex justify-center my-5">
 					<ClineLogoWhite className="size-16" />
 				</div>
