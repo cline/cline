311f109e639b9fef241c8bcc6fedd9f8bfe40f27
Sarah Fortune <fortunes@cline.bot>
2025-04-25 00:47:55 -0700

    Remove ununsed import

--- src/core/controller/account/accountLoginClicked.ts
+++ src/core/controller/account/accountLoginClicked.ts
@@ -1,6 +1,5 @@
 import * as vscode from "vscode"
 import crypto from "crypto"
-import { EmptyRequest } from "../../../shared/proto/common"
 import { Controller } from "../index"
 import { storeSecret } from "../../storage/state"
 
