c967f2c8f0a7b81f6dbfefb34c8c5b1c9a8093ed
canvrno <kevin@cline.bot>
2025-04-29 22:47:53 -0700

    refactored deleteRuleFile for consolidation

--- src/shared/proto/file.ts
+++ src/shared/proto/file.ts
@@ -6,7 +6,7 @@
 
 /* eslint-disable */
 import { BinaryReader, BinaryWriter } from "@bufbuild/protobuf/wire"
-import { Empty, Metadata, OperationResponse, StringRequest } from "./common"
+import { Empty, Metadata, StringRequest } from "./common"
 
 export const protobufPackage = "cline"
 
