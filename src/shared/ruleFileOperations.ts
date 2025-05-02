996cf17c5bef062d08f795b50d4b5a6e92213921
canvrno <kevin@cline.bot>
2025-05-01 17:05:37 -0700

    removed ruleFileOperations

--- src/shared/ruleFileOperations.ts
+++
@@ -1,8 +0,0 @@
-// Re-export the essential types
-import { RuleFileRequest, RuleFile } from "./proto/file"
-
-// Re-export helpers from the conversions layer
-export { DeleteRuleFileRequest, CreateRuleFileRequest } from "./proto-conversions/file"
-
-// Re-export the core types
-export { RuleFileRequest, RuleFile }
