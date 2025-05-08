a1592e5cf6c919cb2bf220345d288ca515c3aef8
Matt Rubens <mrubens@users.noreply.github.com>
2025-05-08 15:01:26 -0400

    Disable Posthog autocapture in the code in addition to in the web console (#3303)

--- webview-ui/src/utils/TelemetryClient.ts
+++ webview-ui/src/utils/TelemetryClient.ts
@@ -17,6 +17,7 @@ class TelemetryClient {
 				loaded: () => posthog.identify(distinctId),
 				capture_pageview: false,
 				capture_pageleave: false,
+				autocapture: false,
 			})
 		} else {
 			TelemetryClient.telemetryEnabled = false
