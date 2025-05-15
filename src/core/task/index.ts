2276923861a91874f29bd9d0cfa20fa29035dad7
canvrno <kevin@cline.bot>
2025-05-15 14:42:35 -0700

    More post rebase fixes

--- src/core/task/index.ts
+++ src/core/task/index.ts
@@ -598,7 +598,9 @@ export class Task {
 	}
 
 	async doesLatestTaskCompletionHaveNewChanges() {
-		if (!this.enableCheckpoints) return false
+		if (!this.enableCheckpoints) {
+			return false
+		}
 
 		const messageIndex = findLastIndex(this.clineMessages, (m) => m.say === "completion_result")
 		const message = this.clineMessages[messageIndex]
