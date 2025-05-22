22431f06f4e1a4b850ddce0cbe964330c98c8516
Chris Estreich <cestreich@gmail.com>
2025-05-22 12:31:55 -0700

    Fix changelog generation (#3859)

--- .changeset/changelog-config.js
+++ .changeset/changelog-config.js
@@ -1,5 +1,3 @@
-// Half-works to simplify the format but needs 'overwrite_changeset_changelog.py' in GHA to finish formatting
-
 const getReleaseLine = async (changeset) => {
 	const [firstLine] = changeset.summary
 		.split("\n")
