ce0f5ba08e8ed1fa3b5d54d13833bf17a17f4d50
Elephant Lumps <celestial_vault@Elephants-MacBook-Pro.local>
2025-05-28 14:56:09 -0700

    add json tool definition and remove extra '.'

--- src/core/prompts/model_prompts/claude4.ts
+++ src/core/prompts/model_prompts/claude4.ts
@@ -130,7 +130,7 @@ Usage:
 </search_files>
 
 ## LS: Lists directory contents
-Description: Lists files and directories in a given path. The path parameter must be an absolute path, not a relative path. You should generally prefer the Glob and Grep tools, if you know which directories to search..
+Description: Lists files and directories in a given path. The path parameter must be an absolute path, not a relative path. You should generally prefer the Glob and Grep tools, if you know which directories to search.
 Usage:
 <function_calls>
 <invoke name="LS">
