ef3ede31a41f64bb95014e3e1506e6eef991dfac
Andrei Eternal <eternal@cline.bot>
2025-06-06 11:25:15 -0700

    protobuf executeQuickWin

--- src/shared/WebviewMessage.ts
+++ src/shared/WebviewMessage.ts
@@ -22,7 +22,6 @@ export interface WebviewMessage {
 		| "grpc_request"
 		| "grpc_request_cancel"
 		| "toggleWorkflow"
-		| "executeQuickWin"
 
 	text?: string
 	disabled?: boolean
@@ -72,8 +71,6 @@ export interface WebviewMessage {
 	workflowPath?: string
 	enabled?: boolean
 	filename?: string
-
-	payload?: { command: string; title: string }
 
 	offset?: number
 	shellIntegrationTimeout?: number
