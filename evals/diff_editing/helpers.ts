507ae084bbe0f5e1a8bcc6f52df2de38e461308d
Toshii <94262432+0xToshii@users.noreply.github.com>
2025-06-09 16:01:27 -0700

    diff edit evals (#4112)
    * base
    
    * multi parallel
    
    * function registry
    
    * nit
    
    * basic prompt
    
    * support data 1
    
    * types
    
    * types
    
    * input format
    
    * path name
    
    * use helpers
    
    * logs
    
    * claude4 prompt
    
    * handling
    
    * evals history
    
    * verbose
    
    * prints
    
    * var
    
    * cli base
    
    * cli inputs
    
    * sqlite
    
    * v3 diff apply
    
    * thinking tokens
    
    * more metrics
    
    * nit
    
    * new structure
    
    * print

---
+++ evals/diff_editing/helpers.ts
@@ -0,0 +1,25 @@
+import { Anthropic } from "@anthropic-ai/sdk"
+
+const formatImagesIntoBlocks = (images?: string[]): Anthropic.ImageBlockParam[] => {
+	return images
+		? images.map((dataUrl) => {
+				// data:image/png;base64,base64string
+				const [rest, base64] = dataUrl.split(",")
+				const mimeType = rest.split(":")[1].split(";")[0]
+				return {
+					type: "image",
+					source: {
+						type: "base64",
+						media_type: mimeType,
+						data: base64,
+					},
+				} as Anthropic.ImageBlockParam
+			})
+		: []
+}
+
+export const formatResponse = {
+	imageBlocks: (images?: string[]): Anthropic.ImageBlockParam[] => {
+		return formatImagesIntoBlocks(images)
+	},
+}
