83dc6607b9092d65d640cc03c9aad1e8dff0622d
Jonas Felix <jonas.felix@felixideas.ch>
2025-06-10 14:32:29 +0200

    set letsboot fork v3.17.12

--- webview-ui/src/components/chat/ChatView.tsx
+++ webview-ui/src/components/chat/ChatView.tsx
@@ -1058,7 +1058,7 @@ const ChatView = ({ isHidden, showAnnouncement, hideAnnouncement, showHistoryVie
 
 	const placeholderText = useMemo(() => {
 		// <letsboot.ch fork change>
-		const text = task ? "Type a message... (letsboot fork v3.17.5)" : "Type your task here... (letsboot fork v3.17.5)"
+		const text = task ? "Type a message... (letsboot fork v3.17.12)" : "Type your task here... (letsboot fork v3.17.12)"
 		// </letsboot.ch fork change>
 		return text
 	}, [task])
