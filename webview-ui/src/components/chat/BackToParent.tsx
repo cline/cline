119554cf31c107e2a03bf276acfc82557a2b329f
WEN-JY <wenjiyong6@gmail.com>
2025-06-17 10:55:44 +0800

    feat: modify parentid logic in task complete part

---
+++ webview-ui/src/components/chat/BackToParent.tsx
@@ -0,0 +1,42 @@
+import SuccessButton from '@/components/common/SuccessButton';
+import { TaskServiceClient } from '@/services/grpc-client';
+import { StringRequest } from '@shared/proto/common';
+import { useExtensionState } from '@/context/ExtensionStateContext';
+
+const headerStyle: React.CSSProperties = {
+  display: "flex",
+  alignItems: "center",
+  gap: "10px",
+  marginBottom: "12px",
+}
+export default function BackToParent() {
+  const { currentTaskItem } =
+    useExtensionState()
+  return currentTaskItem?.parentId && (
+    <>
+      <div
+        style={{
+          ...headerStyle,
+          marginBottom: "10px",
+        }}>
+
+        <div style={{ marginTop: 10 }}>
+          <SuccessButton
+            appearance="secondary"
+            onClick={() => {
+              TaskServiceClient.showTaskWithId(
+                StringRequest.create({
+                  value: currentTaskItem.parentId,
+                }),
+              ).catch((err) =>
+                console.error("Failed to resume task:", err),
+              )
+            }}>
+            <i className="codicon codicon-left" style={{ marginRight: 6 }} />
+            Back to parent Task
+          </SuccessButton>
+        </div>
+      </div>
+    </>
+  )
+}
