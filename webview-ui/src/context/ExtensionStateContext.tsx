db4f7f3320124d88741f2aca7f1e42df38f4cbc2
roomote <chris@roocode.com>
2025-06-18 09:08:54 -0700

    Fix #4837: Default 'Enable MCP Server Creation' to false (#4838)

--- webview-ui/src/context/ExtensionStateContext.tsx
+++ webview-ui/src/context/ExtensionStateContext.tsx
@@ -164,7 +164,7 @@ export const ExtensionStateContextProvider: React.FC<{ children: React.ReactNode
 		terminalOutputLineLimit: 500,
 		terminalShellIntegrationTimeout: 4000,
 		mcpEnabled: true,
-		enableMcpServerCreation: true,
+		enableMcpServerCreation: false,
 		alwaysApproveResubmit: false,
 		requestDelaySeconds: 5,
 		currentApiConfigName: "default",
