3ebcff89c00a85dee60310a70d4018ca0b0b30ee
Sarah Fortune <sarah.fortune@gmail.com>
2025-06-20 13:44:22 -0700

    Support external webview  (#4336)
    * Add ui.getWebviewHtml to the protobus
    
    This will return the HTML content for external clients.
    
    * Add getUri to ExternalWebviewProvider
    
    Change getUri to return URIs for files in an appropriate format for the external web view.
    Use URI from npm module in ExternalWebviewProvider.
    Use a default value for the cline dir, ~/.cline
    Turn off gRPC debugging
    
    * Include node modules used as assets in the standalone package.
    
    * Throw an error if trying to recreate webview panel in standalone app.

--- standalone/runtime-files/vscode/vscode-impls.js
+++ standalone/runtime-files/vscode/vscode-impls.js
@@ -63,13 +63,8 @@ vscode.window = {
 	registerWebviewViewProvider: () => ({ dispose: () => {} }),
 	onDidChangeActiveTextEditor: () => ({ dispose: () => {} }),
 	createTextEditorDecorationType: () => ({ dispose: () => {} }),
-	createWebviewPanel: (...args) => {
-		console.log("Stubbed createWebviewPanel:", ...args)
-		return {
-			webview: {},
-			reveal: () => {},
-			dispose: () => {},
-		}
+	createWebviewPanel: (..._args) => {
+		throw new Error("WebviewPanel is not supported in standalone app.")
 	},
 }
 
