3187b425ab0e9e4b526868b564d8ef61d711a8e5
celestial-vault <58194240+celestial-vault@users.noreply.github.com>
2025-06-24 10:54:23 -0700

    add eslint rule banning postMessage in webview (#4414)

--- eslint-rules/index.js
+++ eslint-rules/index.js
@@ -1,11 +1,13 @@
 // eslint-rules/index.js
 const noProtobufObjectLiterals = require("./no-protobuf-object-literals")
 const noGrpcClientObjectLiterals = require("./no-grpc-client-object-literals")
+const noVscodePostmessage = require("./no-vscode-postmessage")
 
 module.exports = {
 	rules: {
 		"no-protobuf-object-literals": noProtobufObjectLiterals,
 		"no-grpc-client-object-literals": noGrpcClientObjectLiterals,
+		"no-vscode-postmessage": noVscodePostmessage,
 	},
 	configs: {
 		recommended: {
@@ -13,6 +15,7 @@ module.exports = {
 			rules: {
 				"local/no-protobuf-object-literals": "error",
 				"local/no-grpc-client-object-literals": "error",
+				"local/no-vscode-postmessage": "error",
 			},
 		},
 	},
