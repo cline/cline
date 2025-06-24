911595af6f26816358f800d74c4dce1cb2888b43
canvrno <46584286+canvrno@users.noreply.github.com>
2025-06-24 16:05:07 -0700

    [PROTOBUS] Move telemetrySetting to protobus (#3711)
    * telemtrySetting protobus migration
    
    * merge conflcit fix
    
    * Updated telemetrySettingRequest to use create
    
    * rebase/merge fixes

---
+++ src/shared/proto-conversions/state/telemetry-setting-conversion.ts
@@ -0,0 +1,34 @@
+import { TelemetrySettingEnum } from "../../proto/state"
+import { TelemetrySetting } from "../../TelemetrySetting"
+
+/**
+ * Converts a domain TelemetrySetting string to a proto TelemetrySettingEnum
+ */
+export function convertDomainTelemetrySettingToProto(setting: TelemetrySetting): TelemetrySettingEnum {
+	switch (setting) {
+		case "unset":
+			return TelemetrySettingEnum.UNSET
+		case "enabled":
+			return TelemetrySettingEnum.ENABLED
+		case "disabled":
+			return TelemetrySettingEnum.DISABLED
+		default:
+			return TelemetrySettingEnum.UNSET
+	}
+}
+
+/**
+ * Converts a proto TelemetrySettingEnum to a domain TelemetrySetting string
+ */
+export function convertProtoTelemetrySettingToDomain(setting: TelemetrySettingEnum): TelemetrySetting {
+	switch (setting) {
+		case TelemetrySettingEnum.UNSET:
+			return "unset"
+		case TelemetrySettingEnum.ENABLED:
+			return "enabled"
+		case TelemetrySettingEnum.DISABLED:
+			return "disabled"
+		default:
+			return "unset"
+	}
+}
