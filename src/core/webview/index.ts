b03f6831e3ef67b15d98a3c515527b7c080f9d65
canvrno <46584286+canvrno@users.noreply.github.com>
2025-06-24 11:43:05 -0700

    pretty (#4307)

--- src/core/webview/index.ts
+++ src/core/webview/index.ts
@@ -200,7 +200,7 @@ export abstract class WebviewProvider {
 				<link href="${codiconsUri}" rel="stylesheet" />
 				<link href="${katexCssUri}" rel="stylesheet" />
 				<meta http-equiv="Content-Security-Policy" content="default-src 'none';
-					connect-src https://*.posthog.com https://*.firebaseauth.com https://*.firebaseio.com https://*.googleapis.com https://*.firebase.com; 
+					connect-src https://*.posthog.com https://*.cline.bot https://*.firebaseauth.com https://*.firebaseio.com https://*.googleapis.com https://*.firebase.com; 
 					font-src ${this.getCspSource()} data:; 
 					style-src ${this.getCspSource()} 'unsafe-inline'; 
 					img-src ${this.getCspSource()} https: data:; 
