27f8372c5bb8e0fd36dcb1daf6043d1518338ee8
kevinneung <94151024+kevinneung@users.noreply.github.com>
2025-07-01 16:50:02 -0700

    add cline.walkthrough to command handler (#4621)
    added cline.walkthrough to command handler, since it would throw an error otherwise

--- src/extension.ts
+++ src/extension.ts
@@ -640,6 +640,14 @@ export async function activate(context: vscode.ExtensionContext) {
 		}),
 	)
 
+	// Register the openWalkthrough command handler
+	context.subscriptions.push(
+		vscode.commands.registerCommand("cline.openWalkthrough", async () => {
+			await vscode.commands.executeCommand("workbench.action.openWalkthrough", "saoudrizwan.claude-dev#ClineWalkthrough")
+			telemetryService.captureButtonClick("command_openWalkthrough", undefined, true)
+		}),
+	)
+
 	// Register the generateGitCommitMessage command handler
 	context.subscriptions.push(
 		vscode.commands.registerCommand("cline.generateGitCommitMessage", async () => {
