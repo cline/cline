0808bd2bc367cb0d9bb73cc21bfe5b669d41eabe
pashpashpash <nik@cline.bot>
2025-07-10 19:23:43 -0700

    supporting buy_credits_url response from backend

--- webview-ui/src/components/chat/CreditLimitError.tsx
+++ webview-ui/src/components/chat/CreditLimitError.tsx
@@ -9,6 +9,7 @@ interface CreditLimitErrorProps {
 	totalSpent?: number
 	totalPromotions?: number
 	message: string
+	buyCreditsUrl?: string
 }
 
 const CreditLimitError: React.FC<CreditLimitErrorProps> = ({
@@ -16,6 +17,7 @@ const CreditLimitError: React.FC<CreditLimitErrorProps> = ({
 	totalSpent = 0,
 	totalPromotions = 0,
 	message = "You have run out of credit.",
+	buyCreditsUrl = "https://app.cline.bot/dashboard",
 }) => {
 	// We have to divide because the balance is stored in microcredits
 	return (
@@ -28,7 +30,7 @@ const CreditLimitError: React.FC<CreditLimitErrorProps> = ({
 			</div>
 
 			<VSCodeButtonLink
-				href="https://app.cline.bot/"
+				href={buyCreditsUrl}
 				style={{
 					width: "100%",
 					marginBottom: "8px",
