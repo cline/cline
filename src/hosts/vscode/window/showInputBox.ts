563606c8a83d51e68ff2050ca57695a47e918c4b
abeatrix <beatrix@cline.bot>
2025-07-15 18:24:35 -0700

    Simplify

--- src/hosts/vscode/window/showInputBox.ts
+++ src/hosts/vscode/window/showInputBox.ts
@@ -1,14 +1,11 @@
 import * as vscode from "vscode"
-import { ShowInputBoxRequest, SelectedResponse } from "@/shared/proto/index.host"
+import { ShowInputBoxRequest, ShowInputBoxResponse } from "@/shared/proto/index.host"
 
-export async function showInputBox(request: ShowInputBoxRequest): Promise<SelectedResponse | undefined> {
-	const selectedOption = await vscode.window.showInputBox({
+export async function showInputBox(request: ShowInputBoxRequest): Promise<ShowInputBoxResponse | undefined> {
+	const response = await vscode.window.showInputBox({
 		title: request.title,
 		prompt: request.prompt,
 		value: request.value,
-		placeHolder: request.placeHolder,
-		password: request.password,
-		ignoreFocusOut: request.ignoreFocusOut,
 	})
-	return SelectedResponse.create({ selectedOption })
+	return ShowInputBoxResponse.create({ response })
 }
