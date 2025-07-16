3ebc7744ab959a1370ce03b29c7ed552bc41e59b
Sarah Fortune <sarah.fortune@gmail.com>
2025-07-16 09:37:28 -0700

    Add a script to run the standalone service.
    Remove spammy log statements.

---
+++ scripts/runclinecore.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -eu #x
+
+CORE_DIR=~/.cline/core
+INSTALL_DIR=$CORE_DIR/0.0.1
+
+# Build standalone app
+npm run compile-standalone
+
+# Remove old unpacked versions to force reinstall
+rm -rf $CORE_DIR/* || true
+
+mkdir -p $INSTALL_DIR
+cp dist-standalone/standalone.zip $INSTALL_DIR
+cd $INSTALL_DIR
+unp standalone.zip > /dev/null
+
+pkill -f cline-core.js || true
+NODE_PATH=./node_modules node cline-core.js
