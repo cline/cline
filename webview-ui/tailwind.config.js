8b49b5ef9f452cba598ebdd428ea5b5407d879dc
abeatrix <beatrix@cline.bot>
2025-07-16 15:03:55 -0700

    revert accidental changes

--- webview-ui/tailwind.config.js
+++ webview-ui/tailwind.config.js
@@ -1,6 +1,8 @@
+const { heroui } = require("@heroui/react")
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}"],
+	content: ["./src/**/*.{js,ts,jsx,tsx,mdx}", "./node_modules/@heroui/theme/dist/**/*.{js,ts,jsx,tsx}"],
 	theme: {
 		extend: {
 			fontFamily: {
@@ -10,4 +12,16 @@ module.exports = {
 	},
 	// Toggle dark-mode based on .dark class or data-mode="dark"
 	darkMode: ["class", '[data-mode="dark"]', '[class="vs-dark"]'],
+	plugins: [
+		heroui({
+			defaultTheme: "vscode",
+			themes: {
+				vscode: {
+					colors: {
+						background: "",
+					},
+				},
+			},
+		}),
+	],
 }
