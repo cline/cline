a4d3bac66a28b0aebb894bdda0c656a57872a6e3
arafatkatze <arafat.da.khan@gmail.com>
2025-07-17 17:35:17 -0700

    Adding huggingface provider

--- webview-ui/src/components/settings/utils/providerUtils.ts
+++ webview-ui/src/components/settings/utils/providerUtils.ts
@@ -36,6 +36,8 @@ import {
 	liteLlmModelInfoSaneDefaults,
 	moonshotModels,
 	moonshotDefaultModelId,
+	huggingFaceModels,
+	huggingFaceDefaultModelId,
 	nebiusModels,
 	nebiusDefaultModelId,
 	cerebrasModels,
@@ -174,6 +176,12 @@ export function normalizeApiConfiguration(apiConfiguration?: ApiConfiguration): 
 			return getProviderData(xaiModels, xaiDefaultModelId)
 		case "moonshot":
 			return getProviderData(moonshotModels, moonshotDefaultModelId)
+		case "huggingface":
+			return {
+				selectedProvider: provider,
+				selectedModelId: apiConfiguration?.huggingFaceModelId || huggingFaceDefaultModelId,
+				selectedModelInfo: apiConfiguration?.huggingFaceModelInfo || huggingFaceModels[huggingFaceDefaultModelId],
+			}
 		case "nebius":
 			return getProviderData(nebiusModels, nebiusDefaultModelId)
 		case "sambanova":
