600a6e33ed9ea45957e120e2b856a6a3d7f09129
Sarah Fortune <sarah.fortune@gmail.com>
2025-07-20 17:57:22 -0700

    Move duplicated code for loading the protobuf descriptor set into a shared util file. (#5059)
    * Move duplicated code for laoding the protobuf descriptor set into a shared util file.
    
    * Formatting
    
    * Update comment

--- scripts/generate-host-bridge-client.mjs
+++ scripts/generate-host-bridge-client.mjs
@@ -2,11 +2,8 @@
 
 import * as fs from "fs/promises"
 import * as path from "path"
-import * as grpc from "@grpc/grpc-js"
-import * as protoLoader from "@grpc/proto-loader"
 import chalk from "chalk"
-
-const DESCRIPTOR_SET = path.resolve("dist-standalone/proto/descriptor_set.pb")
+import { loadServicesFromProtoDescriptor, getFqn } from "./proto-utils.mjs"
 
 // Contains the interface definitions for the host bridge clients.
 const TYPES_FILE = path.resolve("src/generated/hosts/host-bridge-client-types.ts")
@@ -15,43 +12,11 @@ const EXTERNAL_CLIENT_FILE = path.resolve("src/generated/hosts/standalone/host-b
 // Contains the handler map for the external host bridge clients (using the custom service registry).
 const VSCODE_CLIENT_FILE = path.resolve("src/generated/hosts/vscode/hostbridge-grpc-service-config.ts")
 
-const typeNameToFQN = new Map()
-
-function addTypeNameToFqn(name, fqn) {
-	if (typeNameToFQN.has(name)) {
-		throw new Error(`Proto type ${name} redefined (${fqn}).`)
-	}
-	typeNameToFQN.set(name, fqn)
-}
-function getFqn(name) {
-	if (!typeNameToFQN.has(name)) {
-		throw Error(`No FQN for ${name}`)
-	}
-	return typeNameToFQN.get(name)
-}
 /**
  * Main function to generate the host bridge client
  */
 async function main() {
-	// Load service definitions from descriptor set
-	const descriptorBuffer = await fs.readFile(DESCRIPTOR_SET)
-	const packageDefinition = protoLoader.loadFileDescriptorSetFromBuffer(descriptorBuffer)
-	const proto = grpc.loadPackageDefinition(packageDefinition)
-
-	// Extract host services and proto messages from the proto definition
-	const hostServices = {}
-	for (const [name, def] of Object.entries(proto.host)) {
-		if (def && "service" in def) {
-			hostServices[name] = def
-		} else {
-			addTypeNameToFqn(name, `proto.host.${name}`)
-		}
-	}
-	for (const [name, def] of Object.entries(proto.cline)) {
-		if (def && !("service" in def)) {
-			addTypeNameToFqn(name, `proto.cline.${name}`)
-		}
-	}
+	const { hostServices } = await loadServicesFromProtoDescriptor()
 
 	await generateTypesFile(hostServices)
 	await generateExternalClientFile(hostServices)
