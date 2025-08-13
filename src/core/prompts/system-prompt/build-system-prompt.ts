af274f5d9d0b1d4345e919ce7d8e556e6fff9970
celestial-vault <58194240+celestial-vault@users.noreply.github.com>
2025-08-13 14:25:09 +0200

    update comments in build system prompt

--- src/core/prompts/system-prompt/build-system-prompt.ts
+++ src/core/prompts/system-prompt/build-system-prompt.ts
@@ -17,8 +17,11 @@ export const buildSystemPrompt = async (
 		return SYSTEM_PROMPT_CLAUDE4(cwd, supportsBrowserUse, mcpHub, browserSettings)
 	}
 	// catch all for gemini 2.5, gpt 5, etc.
-	// New prompts per family can be added as granularly as we like by adding a folder in the "families" folder and creating a function to discriminate a group of models in the utils.
+	// New prompts per family can be added as granularly as we like by adding a folder in the "families" folder
+	// We then discriminate between families with a functions in the utils.
 	else if (isNextGenModelFamily(apiHandlerModel)) {
+		// This is a clone of claude4 model family prompt, just laying the groundwork for different prompts per family
+		// NOTE: delete this comment when this prompt is no longer a clone
 		return SYSTEM_PROMPT_NEXT_GEN(cwd, supportsBrowserUse, mcpHub, browserSettings)
 	} else {
 		return GENERIC_SYSTEM_PROMPT(cwd, supportsBrowserUse, mcpHub, browserSettings)
