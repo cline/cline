6c040ac1860173d4ee09a49a14724613a7584c57
frostbournesb <frostbournesb@protonmail.com>
2025-08-15 15:49:33 -0700

    Add review links to About section

--- webview-ui/src/components/settings/sections/AboutSection.tsx
+++ webview-ui/src/components/settings/sections/AboutSection.tsx
@@ -13,11 +13,25 @@ const AboutSection = ({ version, renderSectionHeader }: AboutSectionProps) => {
 			<Section>
 				<div className="text-center text-[var(--vscode-descriptionForeground)] text-xs leading-[1.2] px-0 py-0 pr-2 pb-[15px] mt-auto">
 					<p className="break-words m-0 p-0">
-						If you have any questions or feedback, feel free to open an issue at{" "}
+						<span className="text-xs">If you have any questions or feedback, feel free to open an issue at </span>
 						<VSCodeLink href="https://github.com/cline/cline" className="inline">
 							https://github.com/cline/cline
 						</VSCodeLink>
 					</p>
+					<div className="flex flex-col my-4 text-sm">
+						<span>Want to leave a review?</span>
+						<div>
+							<VSCodeLink
+								href="https://marketplace.visualstudio.com/items?itemName=saoudrizwan.claude-dev&ssr=false#review-details"
+								className="inline">
+								VSCode Marketplace
+							</VSCodeLink>
+							<span className="">•</span>
+							<VSCodeLink href="https://open-vsx.org/extension/saoudrizwan/claude-dev/reviews" className="inline">
+								Open VSX Registry
+							</VSCodeLink>
+						</div>
+					</div>
 					<p className="italic mt-[10px] mb-0 p-0">v{version}</p>
 				</div>
 			</Section>
