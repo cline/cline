01c4ead15548c906cc690ef030ee447720ccb5b0
Sarah Fortune <sarah.fortune@gmail.com>
2025-08-19 14:52:20 -0700

    Add an RPC to the host bridge to return information about the active editor (#5690)
    Add an RPC that returns details about the currently active editor. Right now it just returns the file path.
    
    Update the place where this is used.
    
    Remove commented out code that references `vscode.window.activeTextEditor`.

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -115,7 +115,7 @@ export function getDesktopDir() {
 // Returns the workspace path of the file in the current editor.
 // If there is no open file, it returns the top level workspace directory.
 export async function getWorkspacePath(defaultCwd = ""): Promise<string> {
-	const currentFilePath = vscode.window.activeTextEditor?.document.uri.fsPath
+	const currentFilePath = (await HostProvider.window.getActiveEditor({})).filePath
 	if (!currentFilePath) {
 		return await getCwd(defaultCwd)
 	}
