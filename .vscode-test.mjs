98c44a2825792efaf328f655cfbc7bee1ef331d2
jiclotus <joseignaciocastelli92@gmail.com>
2025-09-12 15:39:12 +0200

    adding new configuration

--- .vscode-test.mjs
+++ .vscode-test.mjs
@@ -5,10 +5,7 @@ export default defineConfig({
 	files: "src/test/*.test.ts",
 	mocha: {
 		ui: "bdd",
-		timeout: 20000, // Maximum time (in ms) that a test can run before failing
-		/** Set up TypeScript compilation and alias path resolution during tests
-		 * @See {@link file://./test-setup-integration.js}
-		 */
+		timeout: 20000,
 		require: ["ts-node/register/transpile-only", "./test-setup-integration.js"],
 	},
 	workspaceFolder: "test-workspace",
@@ -17,5 +14,10 @@ export default defineConfig({
 	launchArgs: ["--disable-extensions"],
 	env: {
 		TS_NODE_PROJECT: "./tsconfig.test.json",
+	},
+	coverage: {
+		reporter: ["lcov", "text"],
+		include: ["src/**/*.ts"],
+		exclude: ["**/*.d.ts", "**/*.test.ts", "**/*.spec.ts", "**/test/**", "**/tests/**", "src/test/**", "src/generated/**"],
 	},
 })
