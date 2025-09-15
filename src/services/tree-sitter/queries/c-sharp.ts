1b4819c086b3aac31cfa4e7f9fe3140228ed2f9f
Mubeen Zulfiqar <mubeen_zulfiqar@yahoo.com>
2025-09-15 23:10:45 +0500

    fix: corrected C# tree-sitter query (#7813)

--- src/services/tree-sitter/queries/c-sharp.ts
+++ src/services/tree-sitter/queries/c-sharp.ts
@@ -3,61 +3,65 @@ C# Tree-Sitter Query Patterns
 */
 export default `
 ; Using directives
-(using_directive) @name.definition.using
-
+(using_directive) @definition.using
+ 
 ; Namespace declarations (including file-scoped)
+; Support both simple names (TestNamespace) and qualified names (My.Company.Module)
 (namespace_declaration
-  name: (identifier) @name.definition.namespace)
+  name: (qualified_name) @name) @definition.namespace
+(namespace_declaration
+  name: (identifier) @name) @definition.namespace
 (file_scoped_namespace_declaration
-  name: (identifier) @name.definition.namespace)
-
+  name: (qualified_name) @name) @definition.namespace
+(file_scoped_namespace_declaration
+  name: (identifier) @name) @definition.namespace
+ 
 ; Class declarations (including generic, static, abstract, partial, nested)
 (class_declaration
-  name: (identifier) @name.definition.class)
-
+  name: (identifier) @name) @definition.class
+ 
 ; Interface declarations
 (interface_declaration
-  name: (identifier) @name.definition.interface)
-
+  name: (identifier) @name) @definition.interface
+ 
 ; Struct declarations
 (struct_declaration
-  name: (identifier) @name.definition.struct)
-
+  name: (identifier) @name) @definition.struct
+ 
 ; Enum declarations
 (enum_declaration
-  name: (identifier) @name.definition.enum)
-
+  name: (identifier) @name) @definition.enum
+ 
 ; Record declarations
 (record_declaration
-  name: (identifier) @name.definition.record)
-
+  name: (identifier) @name) @definition.record
+ 
 ; Method declarations (including async, static, generic)
 (method_declaration
-  name: (identifier) @name.definition.method)
-
+  name: (identifier) @name) @definition.method
+ 
 ; Property declarations
 (property_declaration
-  name: (identifier) @name.definition.property)
-
+  name: (identifier) @name) @definition.property
+ 
 ; Event declarations
 (event_declaration
-  name: (identifier) @name.definition.event)
-
+  name: (identifier) @name) @definition.event
+ 
 ; Delegate declarations
 (delegate_declaration
-  name: (identifier) @name.definition.delegate)
-
+  name: (identifier) @name) @definition.delegate
+ 
 ; Attribute declarations
-(class_declaration
-  (attribute_list
-    (attribute
-      name: (identifier) @name.definition.attribute)))
-
+(attribute
+  name: (identifier) @name) @definition.attribute
+ 
 ; Generic type parameters
-(type_parameter_list
-  (type_parameter
-    name: (identifier) @name.definition.type_parameter))
-
+(type_parameter
+  name: (identifier) @name) @definition.type_parameter
+ 
 ; LINQ expressions
-(query_expression) @name.definition.linq_expression
+(query_expression) @definition.linq_expression
 `
+ 
+ 
