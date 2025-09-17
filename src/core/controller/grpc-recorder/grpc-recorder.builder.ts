b8a23f98e9b8826187a12699574720ac9d9ce6f7
jiclotus <joseignaciocastelli92@gmail.com>
2025-09-17 16:54:12 +0200

    refactoring

--- src/core/controller/grpc-recorder/grpc-recorder.builder.ts
+++ src/core/controller/grpc-recorder/grpc-recorder.builder.ts
@@ -92,9 +92,13 @@ function testFilters(): GrpcRequestFilter[] {
 		(req) => req.is_streaming,
 		(req) => ["cline.UiService", "cline.McpService", "cline.WebService"].includes(req.service),
 		(req) =>
-			["refreshOpenRouterModels", "getAvailableTerminalProfiles", "showTaskWithId", "deleteTasksWithIds"].includes(
-				req.method,
-			),
+			[
+				"refreshOpenRouterModels",
+				"getAvailableTerminalProfiles",
+				"showTaskWithId",
+				"deleteTasksWithIds",
+				"getTotalTasksSize",
+			].includes(req.method),
 	]
 }
 
