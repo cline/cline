448c0c5b090b0963ff539c97a00af3885b03472e
0xtoshii <94262432+0xToshii@users.noreply.github.com>
2025-10-06 11:20:42 -0700

    adding send as a top level command

--- cli/cmd/cline/main.go
+++ cli/cmd/cline/main.go
@@ -49,6 +49,7 @@ monitoring capabilities from the terminal.`,
 	rootCmd.AddCommand(cli.NewInstanceCommand())
 	rootCmd.AddCommand(cli.NewVersionCommand())
 	rootCmd.AddCommand(cli.NewAuthCommand())
+	rootCmd.AddCommand(cli.NewTaskSendCommand())
 
 	if err := rootCmd.ExecuteContext(context.Background()); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
