058d8868942357c5c7476e9c4a0e8e4df6895f06
Eve Killaby <132302818+candieduniverse@users.noreply.github.com>
2025-10-20 16:10:54 -0700

    Un-hard-code FeatureFlagsServive::getHooksEnabled()

--- src/services/feature-flags/FeatureFlagsService.ts
+++ src/services/feature-flags/FeatureFlagsService.ts
@@ -87,7 +87,7 @@ export class FeatureFlagsService {
 	}
 
 	public getHooksEnabled(): boolean {
-		return this.getBooleanFlagEnabled(FeatureFlag.HOOKS)
+		return this.getBooleanFlagEnabled(FeatureFlag.HOOKS, false)
 	}
 
 	/**
