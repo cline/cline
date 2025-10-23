7ef17ed3f9c98d209fe04d9b16d629a160079685
Eve Killaby <132302818+candieduniverse@users.noreply.github.com>
2025-10-23 14:34:40 -0700

    Refactored away loadTaskStateWithoutWorkflow

--- src/services/feature-flags/FeatureFlagsService.ts
+++ src/services/feature-flags/FeatureFlagsService.ts
@@ -87,7 +87,7 @@ export class FeatureFlagsService {
 	}
 
 	public getHooksEnabled(): boolean {
-		return true //this.getBooleanFlagEnabled(FeatureFlag.HOOKS, false)
+		return this.getBooleanFlagEnabled(FeatureFlag.HOOKS, false)
 	}
 
 	/**
