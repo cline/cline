c451b0a94c370dcd278743099028ac368a24061b
Eve Killaby <132302818+candieduniverse@users.noreply.github.com>
2025-10-24 12:11:24 -0700

    remove cancelHookExecution() in favor of cancelTask()

--- src/services/feature-flags/FeatureFlagsService.ts
+++ src/services/feature-flags/FeatureFlagsService.ts
@@ -83,7 +83,7 @@ export class FeatureFlagsService {
 	}
 
 	public getHooksEnabled(): boolean {
-		return this.getBooleanFlagEnabled(FeatureFlag.HOOKS, false)
+		return true //this.getBooleanFlagEnabled(FeatureFlag.HOOKS, false)
 	}
 
 	public getNativeToolCallEnabled(): boolean {
