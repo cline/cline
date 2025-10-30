3f70406a1cfba777ea09c10694d86c64c423ff9c
Saoud Rizwan <7799382+saoudrizwan@users.noreply.github.com>
2025-10-29 17:33:24 -0700

    Fix removing max requests

--- src/core/task/index.ts
+++ src/core/task/index.ts
@@ -1119,7 +1119,6 @@ export class Task {
 			includeFileDetails = false // we only need file details the first time
 
 			//  The way this agentic loop works is that cline will be given a task that he then calls tools to complete. unless there's an attempt_completion call, we keep responding back to him with his tool's responses until he either attempt_completion or does not use anymore tools. If he does not use anymore tools, we ask him to consider if he's completed the task and then call attempt_completion, otherwise proceed with completing the task.
-			// There is a MAX_REQUESTS_PER_TASK limit to prevent infinite requests, but Cline is prompted to finish the task as efficiently as he can.
 
 			//const totalCost = this.calculateApiCost(totalInputTokens, totalOutputTokens)
 			if (didEndLoop) {
