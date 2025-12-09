f8d909f92f6323cc8bba556268c03fb9713b8a67
King-David Eroh <kderoh5@gmail.com>
2025-12-09 16:46:11 +0100

    Fixed jetbrains test failures

--- src/core/controller/models/updateApiConfigurationProto.ts
+++ src/core/controller/models/updateApiConfigurationProto.ts
@@ -75,6 +75,9 @@ export async function updateApiConfigurationProto(
 			planModeAihubmixModelInfo: protoApiConfiguration.planModeAihubmixModelInfo
 				? fromProtobufOpenAiCompatibleModelInfo(protoApiConfiguration.planModeAihubmixModelInfo)
 				: undefined,
+			planModeIoIntelligenceModelInfo: protoApiConfiguration.planModeIoIntelligenceModelInfo
+				? fromProtobufModelInfo(protoApiConfiguration.planModeIoIntelligenceModelInfo)
+				: undefined,
 
 			// Act Mode
 			actModeOpenRouterModelInfo: protoApiConfiguration.actModeOpenRouterModelInfo
@@ -109,6 +112,9 @@ export async function updateApiConfigurationProto(
 				: undefined,
 			actModeAihubmixModelInfo: protoApiConfiguration.actModeAihubmixModelInfo
 				? fromProtobufOpenAiCompatibleModelInfo(protoApiConfiguration.actModeAihubmixModelInfo)
+				: undefined,
+			actModeIoIntelligenceModelInfo: protoApiConfiguration.actModeIoIntelligenceModelInfo
+				? fromProtobufModelInfo(protoApiConfiguration.actModeIoIntelligenceModelInfo)
 				: undefined,
 			geminiPlanModeThinkingLevel: protoApiConfiguration.geminiPlanModeThinkingLevel,
 			geminiActModeThinkingLevel: protoApiConfiguration.geminiActModeThinkingLevel,
