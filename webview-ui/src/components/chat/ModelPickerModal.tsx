631a7d6566f3c3053cbb68f68e891ab286096d96
Tomás Barreiro <52393857+BarreiroT@users.noreply.github.com>
2026-01-14 09:22:06 -0300

    fix: remotely configured providers - allow switching between remote configured providers and fix chat modal display (#8117)
    * Allow switching between remote configured providers and only display valid providers
    
    * Add changeset
    
    * Return the provider set by the remote config
    
    * Address comments
    
    * Address comment
    
    * Validate when updating settings
    
    * Refactor
    
    * Revert
    
    * Use a more descriptive name
    
    * Fix types
    
    * Check we have remote configured providers, not only that the array is there

--- webview-ui/src/components/chat/ModelPickerModal.tsx
+++ webview-ui/src/components/chat/ModelPickerModal.tsx
@@ -95,6 +95,7 @@ const ModelPickerModal: React.FC<ModelPickerModalProps> = ({ isOpen, onOpenChang
 		showMcp,
 		showHistory,
 		showAccount,
+		remoteConfigSettings,
 		favoritedModelIds,
 		basetenModels,
 		liteLlmModels,
@@ -158,8 +159,12 @@ const ModelPickerModal: React.FC<ModelPickerModalProps> = ({ isOpen, onOpenChang
 
 	// Get configured providers
 	const configuredProviders = useMemo(() => {
+		if (remoteConfigSettings?.remoteConfiguredProviders?.length) {
+			return remoteConfigSettings.remoteConfiguredProviders
+		}
+
 		return getConfiguredProviders(apiConfiguration)
-	}, [apiConfiguration])
+	}, [apiConfiguration, remoteConfigSettings?.remoteConfiguredProviders])
 
 	// Get models for current provider
 	const allModels = useMemo((): ModelItem[] => {
