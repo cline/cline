3ce4e388994ebc396dc897421023ebf807d4b1b9
abeatrix <beatrix@cline.bot>
2026-01-23 21:28:09 -0800

    set storage backup

--- cli-ts/src/providers/secrets.ts
+++ cli-ts/src/providers/secrets.ts
@@ -1,36 +1,37 @@
 import keytar from "keytar"
-import { ClineStorage } from "@/shared/storage"
+import path from "path"
+import { ClineFileStorage, ClineStorage } from "@/shared/storage"
+import { printInfo } from "../utils/display"
 
 export class ClineCredentialStorage extends ClineStorage {
 	private cache: Map<string, string> = new Map()
+	private backupStorage: ClineStorage | null = null
 
-	constructor(private readonly service: string) {
+	constructor(
+		private readonly service: string,
+		private readonly backupDir: string,
+	) {
 		super()
-	}
 
-	public static async ok(): Promise<boolean> {
-		try {
-			// Test connection to keytar service
-			await keytar.findPassword("cline-test-random")
-			return true
-		} catch {
-			// Cannot connect to keytar service
-			return false
-		}
+		keytar
+			.findCredentials(service)
+			.then((c) => {
+				printInfo(`[ClineCredentialStorage] Loaded ${JSON.stringify(c)} credentials from system store`)
+				c.forEach((cred) => {
+					this.cache.set(cred.account, cred.password)
+				})
+			})
+			.catch(() => {
+				this.backupStorage = new ClineFileStorage(path.join(this.backupDir, "auth.json"))
+			})
 	}
 
 	override async _get(key: string): Promise<string | undefined> {
 		try {
-			if (this.cache.has(key)) {
-				return this.cache.get(key)
+			if (this.backupStorage) {
+				return await this.backupStorage.get(key)
 			}
-
-			const secret = await keytar.getPassword(this.service, key)
-			if (secret) {
-				this.cache.set(key, secret)
-			}
-
-			return secret || undefined
+			return this.cache.get(key) || undefined
 		} catch (error) {
 			throw error
 		}
@@ -41,6 +42,11 @@ export class ClineCredentialStorage extends ClineStorage {
 			this.delete(key)
 		}
 		try {
+			if (this.backupStorage) {
+				await this.backupStorage.store(key, value)
+				return
+			}
+
 			await keytar.setPassword(this.service, key, value)
 			this.cache.set(key, value)
 		} catch (error) {
@@ -53,6 +59,12 @@ export class ClineCredentialStorage extends ClineStorage {
 			if (!this.cache.has(key)) {
 				return
 			}
+
+			if (this.backupStorage) {
+				await this.backupStorage.delete(key)
+				return
+			}
+
 			await keytar.deletePassword(this.service, key)
 			this.cache.delete(key)
 		} catch (error) {
