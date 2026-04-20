baa02eea0ce70d53463669bb67c7ec5b18a206c0
raidengr40 <alberto.digital25@gmail.com>
2026-04-20 16:10:14 -0600

    feat(ui): add barrel export for toolbar-popups

---
+++ webview-ui/src/components/chat/toolbar-popups/index.ts
@@ -0,0 +1,3 @@
+export { default as AddContextModal } from "./AddContextModal"
+export { default as ModelSelectorDropdown } from "./ModelSelectorDropdown"
+export { default as PlusPopup } from "./PlusPopup"
