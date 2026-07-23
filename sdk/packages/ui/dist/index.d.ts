0755b72e2c5f6381df89eb858e72e0847a4660b7
John Choi <john.choi@cline.bot>
2026-07-23 13:07:44 -0700

    fix(ui): close unavailable comboboxes

---
+++ sdk/packages/ui/dist/index.d.ts
@@ -0,0 +1,4 @@
+export { Button, type ButtonProps } from "./button.js";
+export { SearchCombobox, type SearchComboboxOption, type SearchComboboxProps, } from "./search-combobox.js";
+export { SessionStatus, type SessionStatusProps, type SessionStatusTone, } from "./session-status.js";
+//# sourceMappingURL=index.d.ts.map
---
+++ sdk/packages/ui/dist/index.js
@@ -0,0 +1,4 @@
+export { Button } from "./button.js";
+export { SearchCombobox, } from "./search-combobox.js";
+export { SessionStatus, } from "./session-status.js";
+//# sourceMappingURL=index.js.map
