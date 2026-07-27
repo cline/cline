4315517fca5228a403d308b4737fda7a5a82c0a2
Dominic Cooney <dominic.cooney@cline.bot>
2026-07-27 10:47:20 -0700

    Stream driver and user events to the observatory via qbt.

--- sdk/packages/core/src/index.ts
+++ sdk/packages/core/src/index.ts
@@ -989,6 +989,8 @@ export {
 	createComputerUseToolFromEnv,
 	GET_DISPLAY_INFO_ACTION,
 	isComputerUseResponse,
+	PUBLISH_EVENT_ACTION,
+	resolveComputerUseTargetFromEnv,
 	type ComputerUseToolOptions,
 } from "./extensions/computer-use";
 export {
@@ -1002,6 +1004,9 @@ export {
 	type ArtifactSinkStatus,
 	ComputerTaskArtifactRecorder,
 	type ComputerTaskArtifactEvent,
+	createJournalEventSink,
+	createTranscriptRecordingHooks,
+	type JournalPublishTransport,
 } from "./extensions/computer-observability";
 export {
 	COMPUTER_USER_PROMPT_VERSION,
